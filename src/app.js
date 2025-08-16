5ef3634f95823717d9c2525952dff73680932929
Yashwanth45-jpg <myashwanth45@gmail.com>
2025-08-16 22:07:47 +0530

    Updatec

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Webhook } = require('svix');
 const mongoose = require('mongoose');
-const User = require('./src/models/user.model.js');
+const User = require('./src/models/user.models.js');
 
 const app = express();
 
